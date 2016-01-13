23a6563da550433195120ac3758cc2179eca9427
carlosyasu91 <carlosyasu91@gmail.com>
2016-01-13 15:22:04 -0800

    Implemented Queue structure

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,8 @@
 var Queue = function() {
   var someInstance = {};
+  var queueSize = 0;
+  var nextIn = 0;
+  var nextOut = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +10,23 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[nextIn] = value;
+    nextIn++;
+    queueSize++;
   };
 
   someInstance.dequeue = function() {
+    if(queueSize){
+      queueSize--;
+      var dequeued = storage[nextOut];
+      delete storage[nextOut];
+      nextOut++;
+      return dequeued;
+    }
   };
 
   someInstance.size = function() {
+    return queueSize;
   };
 
   return someInstance;
