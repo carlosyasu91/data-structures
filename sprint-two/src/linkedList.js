0c2daca0671936e8a8489fb99f48ffa86893e7c3
carlosyasu91 <carlosyasu91@gmail.com>
2016-01-14 10:49:28 -0800

    Implemented linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,13 +3,41 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+  // list = { head: {value: x, next: {value: x, next: undefined } } } } }
+
   list.addToTail = function(value) {
+    var newNode = Node(value);
+
+    if (!list.head){
+      list.head = newNode;
+    }
+
+    if (list.tail) {
+      list.tail.next = newNode;
+    }
+
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    if (list.head) {
+      var oldHead = list.head.value;
+      list.head = list.head.next;
+      return oldHead;
+    }
   };
 
   list.contains = function(target) {
+    var current = list.head;
+
+    while(current !== null){
+      if(current.value === target){
+        return true;
+      }
+      current = current.next;
+    }
+
+    return false;
   };
 
   return list;
@@ -27,3 +55,5 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// Insertion/deletion are constant (O(1)) and access/search are linear (O(n))
