63084bc9c5b677172114d6d1a6d8c308a2bca86a
carlosyasu91 <carlosyasu91@gmail.com>
2016-01-14 21:59:38 -0800

    Implemented HashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,19 +2,42 @@
 
 var HashTable = function() {
   this._limit = 8;
+  this._length = 0;
   this._storage = LimitedArray(this._limit);
+  this._hash = getIndexBelowMaxForKey;
 };
 
 HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var self = this;
+  var index = this._hash(k, this._limit);
+  this._storage.set(index, v);
+  this._length++;
+  // if(this._length > this._limit * 0.75) {
+  //   this._limit *= 2;
+  //   this._storage.each(function(el, i, storage){
+  //     var newIndex = self._hash(i, self._limit);
+  //     self._storage.set(newIndex, el);
+  //   });
+  // }
 };
 
 HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = this._hash(k, this._limit);
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var self = this;
+  var index = this._hash(k, this._limit);
+  this._storage.set(index, undefined);
+  this._length--;
+  // if (this._length < this._limit * 0.25) {
+  //   this._limit /= 2;
+  //   this._storage.each(function(el, i, storage){
+  //     var newIndex = self._hash(i, self._limit);
+  //     self._storage.set(newIndex, el);
+  //   });
+  // }
 };
 
 
