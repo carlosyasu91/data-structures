12765ff2705ff9b2bd23e0b69e486612f97a2c0e
carlosyasu91 <carlosyasu91@gmail.com>
2016-01-15 11:58:01 -0800

    Setup structure for binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,25 @@
 var BinarySearchTree = function(value) {
+  
+  var newBinarySearchTree = Object.create(BinarySearchTree.prototype);
+
+  newBinarySearchTree.value = value;
+  newBinarySearchTree.left = null;
+  newBinarySearchTree.right = null;
+
+  return newBinarySearchTree;
 };
 
+BinarySearchTree.prototype.insert = function(value){
+
+};
+
+BinarySearchTree.prototype.contains = function(value){
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(callback){
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
