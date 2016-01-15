f4af6ddd55f0a0986c20388de725a80200d9bce7
carlosyasu91 <carlosyasu91@gmail.com>
2016-01-14 19:59:34 -0800

    Implemented (basic) set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,25 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = true;
 };
 
 setPrototype.contains = function(item) {
+  return (this._storage.hasOwnProperty(item)) ? true : false;
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// For this only-string version, all functions execute in constant time (O(1))
