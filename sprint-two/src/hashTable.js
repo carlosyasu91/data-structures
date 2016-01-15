1a2066460007efd86fa62f42089905f92d294438
carlosyasu91 <carlosyasu91@gmail.com>
2016-01-15 11:44:23 -0800

    Added resizing functionality, refactored code, added time complexity information

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,54 +7,69 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
-  var self = this;
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index) || [];
-  var tuple = [k ,v];
   
-  bucket.push(tuple);
+  var t = 0;
+  while(bucket[t] && k !== bucket[t][0]){
+    t++;
+  }
+  bucket[t] = [k ,v];
+  
   this._storage.set(index, bucket);
   this._length++;
 
-  // if(this._length > this._limit * 0.75) {
-  //   this._limit *= 2;
-  //   this._storage.each(function(el, i, storage){
-  //     var newIndex = self._hash(i, self._limit);
-  //     self._storage.set(newIndex, el);
-  //   });
-  // }
+  if(this._length > this._limit * 0.75) {
+    this.rehash(this._limit * 2);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
-  var result;
-  bucket.forEach(function(value){
-    if(value[0] === k){
-      result = value[1];
-    }
-  });
-  return result;
+  if(!bucket) {
+    return undefined;
+  }
+
+  var t = 0;
+  while(bucket[t] && k !== bucket[t][0]){
+    t++;
+  }
+
+  return (bucket[t]) ? bucket[t][1] : undefined; 
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
-  var result;
-  bucket.forEach(function(value, i){
-    if(value[0] === k){
-      result = i;
+  
+  var t = 0;
+  while(bucket[t] && k !== bucket[t][0]){
+    t++;
+  }
+  bucket.splice(t, 1);
+  this._length--;
+
+  if(this._length < this._limit * 0.25) {
+    this.rehash(this._limit / 2);
+  }
+};
+
+HashTable.prototype.rehash = function(newLimit) {
+  var currentTable = this._storage;
+  var self = this;
+  this._limit = newLimit;
+  this._length = 0;
+  this._storage = LimitedArray(this._limit);
+
+  currentTable.each(function(bucket){
+    if(!bucket) {
+      return;
     }
+    bucket.forEach(function(tuple){
+      self.insert(tuple[0], tuple[1]);
+    });
   });
-  bucket.splice(result, 1);
-  this.length--;
-  // if (this._length < this._limit * 0.25) {
-  //   this._limit /= 2;
-  //   this._storage.each(function(el, i, storage){
-  //     var newIndex = self._hash(i, self._limit);
-  //     self._storage.set(newIndex, el);
-  //   });
-  // }
 };
 
 
@@ -63,4 +78,5 @@ HashTable.prototype.remove = function(k) {
  * Complexity: What is the time complexity of the above functions?
  */
 
+// At best, .insert, .retrieve and .remove are constant O(1) time, at worst, linear O(n)
 
