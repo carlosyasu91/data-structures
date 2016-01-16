ff06e7757047dbda171e3a4c9531bef904dd792f
carlosyasu91 <carlosyasu91@gmail.com>
2016-01-15 16:08:05 -0800

    Implemented .contains and .depthFirstLog on binarySearchTree, completing sprint-two

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -9,7 +9,7 @@ var BinarySearchTree = function(value) {
   return newBinarySearchTree;
 };
 
-BinarySearchTree.prototype.insert = function(value){
+BinarySearchTree.prototype.insert = function(value){  
   if (value > this.value) {
     if(!this.right){
       this.right = BinarySearchTree(value);
@@ -26,13 +26,28 @@ BinarySearchTree.prototype.insert = function(value){
 };
 
 BinarySearchTree.prototype.contains = function(value){
-
+  if (value === this.value) {
+    return true;
+  } else if (value > this.value && this.right) {
+    return this.contains.call(this.right, value);
+  } else if (value < this.value && this.right) {
+    return this.contains.call(this.left, value);
+  }
+  return false;
 };
 
 BinarySearchTree.prototype.depthFirstLog = function(callback){
-
+  callback(this.value);
+  if(this.left) {
+    this.depthFirstLog.call(this.left, callback);
+  }
+  if(this.right) {
+    this.depthFirstLog.call(this.right, callback);
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// .depthFirst is linear O(n), but .insert and .contains are logarhythmic O(log n)
